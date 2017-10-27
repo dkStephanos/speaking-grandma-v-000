a910d1a3fae6ac5f2312262956fcbcdc955a0800
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 15:31:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(greeting)
 
-  if greeting == greeting.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif greeting == "I LOVE YOU GRANDMA!"
+  if greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif greeting == greeting.upcase
+      return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
